a47d6bcbf2ca56e52ef7b35aa37d3ca5968cd509
madma][ <maxim090491@gmail.com>
2018-04-15 19:59:06 +0300

    Add two classes: listRcp and AddYOR

--- app/src/main/java/com/lucky/max/easytocook/ListRecipes.java
+++ app/src/main/java/com/lucky/max/easytocook/ListRecipes.java
@@ -2,11 +2,18 @@ package com.lucky.max.easytocook;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ListRecipes extends ListActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    private ArrayAdapter<String> mAdapter; //адаптер для списка блюд
+public class ListRecipes extends ListActivity implements AdapterView.OnItemLongClickListener {
+
+
 
     final String[] listRecipe = new String[] {
             "Самса",
@@ -26,14 +33,42 @@ public class ListRecipes extends ListActivity {
 
     };
 
+    private ArrayAdapter<String> mAdapter; //адаптер для списка блюд
+    private ArrayList<String>arrayListRecipe  = new ArrayList<>(Arrays.asList(listRecipe));
+    //конвертация из обычного массива в коллекци
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mAdapter = new ArrayAdapter<>(this, //адаптер для чтения из массива
-                android.R.layout.simple_list_item_1, listRecipe);
+                android.R.layout.simple_list_item_1, arrayListRecipe);
 
         setListAdapter(mAdapter); //запуск адаптера
+        getListView().setOnItemLongClickListener(this);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) { //работа с нажатиями
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(getApplicationContext(),
+                "Вы выбрали " + (position + 0) + " элемент", Toast.LENGTH_SHORT).show();
 
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) { //обработчик долгих нажатий
+        String selectedItem = parent.getItemAtPosition(position).toString();
+
+        mAdapter.remove(selectedItem); // удаляем из эррей листа
+        mAdapter.notifyDataSetChanged(); // сохранить изменения
+
+        Toast.makeText(getApplicationContext(),
+                selectedItem + " удалён.",
+                Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
